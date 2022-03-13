52c39590accf86f3b4b9450bbc78bfcabdac068b
Danny Rakita <djrakita@gmail.com>
2022-03-13 15:52:04 -0500

    added latex support for docs

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+
+//! Optima is an easy to set up and easy to use toolbox for applied planning and optimization.
+//! Its primary use-case is robot motion generation (e.g., motion planning, optimization-based inverse kinematics, etc),
+//! though its underlying structures are general and can apply to many problem spaces.
+//! The core library is written in Rust, though high quality ports to high-level languages such as
+//! Python and Javascript are available via PyO3 and WebAssembly, respectively.
+
